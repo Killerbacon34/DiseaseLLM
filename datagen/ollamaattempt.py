c24a99d5b8b30e94fd5c923d1986aa20e6ef1a70
Bryant-Huang-BU <115976062+Bryant-Huang-BU@users.noreply.github.com>
2025-02-13 23:36:54 -0600

    oof

--- datagen/ollamaattempt.py
+++ datagen/ollamaattempt.py
@@ -1,7 +1,7 @@
 import ollama
 
 MODEL = "deepseek-r1:7b"
-PROMPT = "List 2-3 definite symptoms and some situational symptoms for Prostate Cancer, where each listed symptom, no matter the importance, is delineated by a slash. Do not respond with any reasoning, I just want the symptoms. Do not list which symptoms are definite or not, just the symptoms."
+PROMPT = "List 2-3 definite symptoms and situational symptoms for prostate cancer, where each listed symptom, no matter the importance, is delineated by a slash. Do not respond with any reasoning, I just want the symptoms. Do not list which symptoms in any other way besides symptom/symptom/symptom" 
 
 result = ollama.generate(MODEL, PROMPT)
 response = result["response"]
