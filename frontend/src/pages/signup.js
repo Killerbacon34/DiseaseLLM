b70bd890c1ea5bbae50df6a6afd2325ebd938d15
Bryant-Huang-BU <115976062+Bryant-Huang-BU@users.noreply.github.com>
2025-02-27 02:20:46 -0600

    try 2

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Fingerprint2 from 'fingerprintjs2';
+import { useRouter } from 'next/navigation';
 
 const Signup = () => {
     const router = useRouter();
